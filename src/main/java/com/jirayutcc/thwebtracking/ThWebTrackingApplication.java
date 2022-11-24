a1c096b3a9fa1ea53ae0121da96c11aaad400661
jirayut cc <jirayut_ssww@hotmail.com>
2022-11-24 12:48:40 +0700

    init

---
+++ src/main/java/com/jirayutcc/thwebtracking/ThWebTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.jirayutcc.thwebtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThWebTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThWebTrackingApplication.class, args);
+	}
+
+}
